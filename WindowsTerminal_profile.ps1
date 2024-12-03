a57759ea0f8a2dc9ca20fce70d32bfb9fada978c
XenWayne <xenwayne@mevisual.top>
2024-12-03 09:07:39 +0800

    update css

--- WindowsTerminal_profile.ps1
+++ WindowsTerminal_profile.ps1
@@ -1,11 +1,1 @@
-oh-my-posh init pwsh --config C:\Users\16951\AppData\Local\Programs\oh-my-posh\themes\xwcustom.omp.json | Invoke-Expression
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/montys.omp.json" | Invoke-Expression
